f525000626b601bc858d576ec310170099e6b0ef
piotr <piotr0brodziak@gmail.com>
2023-08-18 19:30:44 +0200

    handle console exception

--- modules/identity/identity-app/src/main/java/com/example/hexsample/app/console/ConsoleController.java
+++ modules/identity/identity-app/src/main/java/com/example/hexsample/app/console/ConsoleController.java
@@ -2,12 +2,14 @@ package com.example.hexsample.app.console;
 
 import com.example.hexsample.domain.model.User;
 import com.example.hexsample.domain.port.in.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.Scanner;
 
 @Controller
+@Slf4j
 public class ConsoleController {
     private final UserService userService;
     Scanner scanner;
@@ -18,35 +20,39 @@ public class ConsoleController {
     }
 
     public void startConsole() {
-        Scanner scanner = new Scanner(System.in);
+        try {
+            Scanner scanner = new Scanner(System.in);
 
-        boolean exit = false;
+            boolean exit = false;
+            while (!exit) {
+                System.out.println("1. Display Users");
+                System.out.println("2. Get User by Email");
+                System.out.println("3. Exit");
+                System.out.print("Choose an option: ");
 
-        while (!exit) {
-            System.out.println("1. Display Users");
-            System.out.println("2. Get User by Email");
-            System.out.println("3. Exit");
-            System.out.print("Choose an option: ");
+                int option = scanner.nextInt();
+                scanner.nextLine();  // Consume the newline character
 
-            int option = scanner.nextInt();
-            scanner.nextLine();  // Consume the newline character
-
-            switch (option) {
-                case 1:
-                    displayUsers();
-                    break;
-                case 2:
-                    getUserByEmail();
-                    break;
-                case 3:
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Invalid option.");
+                switch (option) {
+                    case 1:
+                        displayUsers();
+                        break;
+                    case 2:
+                        getUserByEmail();
+                        break;
+                    case 3:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Invalid option.");
+                }
             }
-        }
 
-        scanner.close();
+            scanner.close();
+        } catch (Exception exception) {
+            log.warn("Invalid value, continue use app");
+            startConsole();
+        }
     }
 
     private void displayUsers() {
