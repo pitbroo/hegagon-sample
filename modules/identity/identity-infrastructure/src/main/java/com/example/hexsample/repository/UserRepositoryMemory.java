123f931d65f434ea2cf80643a9a86a35a74f0b38
piotr <piotr0brodziak@gmail.com>
2023-08-18 19:31:22 +0200

    mock items in memory repository

--- modules/identity/identity-infrastructure/src/main/java/com/example/hexsample/repository/UserRepositoryMemory.java
+++ modules/identity/identity-infrastructure/src/main/java/com/example/hexsample/repository/UserRepositoryMemory.java
@@ -2,23 +2,36 @@ package com.example.hexsample.repository;
 
 import com.example.hexsample.domain.model.User;
 import com.example.hexsample.domain.port.out.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.val;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 class UserRepositoryMemory implements UserRepository {
+    List<User> users;
+    @PostConstruct
+    void onInit(){
+        users = List.of(
+                new User("Piotr", "Kowalski", 27, "piotr@wp.pl",
+                        "+48602891000", "908001233200"),
+                new User("Mariusz", "Pudzikowski", 47, "mariusz@wp.pl",
+                        "+48601891000", "778001233200")
+        );
+
+    }
     @Override
-    public User getByEmail() {
-        return new User("Piotr", "Kowalski", 27, "piotr@wp.pl",
-                "+48602891000", "908001233200");
+    public Optional<User> getByEmail(String email) {
+         val userList = users.stream().filter(user -> user.email().equals(email)).toList();
+         if (userList.size() > 0){
+             return Optional.of(userList.get(0));
+         }else return Optional.empty();
     }
 
     @Override
     public List<User> getUsers() {
-        return List.of(
-                new User("Piotr", "Kowalski", 27, "piotr@wp.pl",
-                        "+48602891000", "908001233200")
-        );
+        return users;
     }
 }
